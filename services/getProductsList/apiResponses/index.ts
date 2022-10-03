400d175f1e604cd4ef36c9587268abf70b1beeee
Eduardo Neto <Eduardo_Neto@epam.com>
2022-10-03 17:35:25 +0100

    backend initial setup and getProductList lambda

---
+++ services/getProductsList/apiResponses/index.ts
@@ -0,0 +1,14 @@
+export const apiResponses = {
+    _200: (body: {[key: string]: unknown}) => {
+        return {
+            statusCode: 200,
+            body: JSON.stringify(body, null, 2),
+        }
+    },
+    _400: (body: {[key: string]: unknown}) => {
+        return {
+            statusCode: 400,
+            body: JSON.stringify(body, null, 2),
+        }
+    }
+}
---
+++ services/getProductsList/types/index.ts
@@ -0,0 +1,6 @@
+export type Product = {
+    id: string | undefined;
+    title: string;
+    description: string;
+    price: number;
+}
